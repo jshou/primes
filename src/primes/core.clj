6c8fa84f15d71f616f4392f664b4d65566c0c911
jshou <joshuahou@gmail.com>
2012-07-05 09:27:52 -0700

    initial commit

---
+++ src/primes/core.clj
@@ -0,0 +1,7 @@
+(ns primes.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/primes/core_test.clj
@@ -0,0 +1,7 @@
+(ns primes.core-test
+  (:use clojure.test
+        primes.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
