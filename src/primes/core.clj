9ec8d969a2b97817bdf20d3e0e51133ffddd74e1
jshou <joshuahou@gmail.com>
2012-07-07 19:10:43 -0700

    using function overloading

--- src/primes/core.clj
+++ src/primes/core.clj
@@ -1,23 +1,22 @@
 (ns primes.core
   (:gen-class))
 
-(defn- next-prime [i primes] ; i cannot be 1
+(defn next-prime [i primes] ; i cannot be 1
   (if (empty? primes)
     2
     (if (not-any? #(= 0 (mod i %)) primes)
       i
       (next-prime (+ 1 i) primes))))
 
-(defn- first-n-primes-foo [n primes]
-  (if (= n (count primes))
-    primes
-    (let [nextp (next-prime (last primes) primes)]
-      (first-n-primes-foo n (conj primes nextp)))))
-
 (defn first-n-primes
- "returns the first n primes"
- [n]
- (first-n-primes-foo n []))
+  "returns the first n primes"
+  ([n]
+    (first-n-primes n []))
+  ([n primes] ; private recursive helper
+    (if (= n (count primes))
+      primes
+      (let [nextp (next-prime (last primes) primes)]
+        (first-n-primes n (conj primes nextp))))))
 
 (defn -main
   [& args]
